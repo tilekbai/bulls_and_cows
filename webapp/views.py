31d5404e61f8f9f65e7396a5e322ad0cec68679a
Tilekbai <tilek009@mail.ru>
2021-02-23 22:06:16 +0900

    win.html was created

--- webapp/views.py
+++ webapp/views.py
@@ -1,34 +1,56 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 
 from django.shortcuts import render
 
+secret_nums = []  
 def index_view(request):
-    # return render(request, 'index.html')
+
+
+    while len(secret_nums) != 4:
+        rand = (randint(1, 10))
+        if rand not in secret_nums:
+            secret_nums.append(rand)
+
     if request.method == 'GET':
         return render(request, 'index.html')
-    
+
 
     elif request.method == 'POST':
         
-        context_calc = {
+        numbers_dict = {
             'first_number':request.POST.get('first_number'),
             'second_number':request.POST.get('second_number'),
-            'operation':request.POST.get('operation'),
-            'res' : 0,
-            'sign' : '0',
+            'third_number':request.POST.get('third_number'),
+            'fourth_number':request.POST.get('fourth_number'),
             }
 
-        try:
-            numbers = (list(map(int, request_body['numbers'][0].split(' '))))
-            response_body += f'<div>{", ".join(list(map(str, numbers)))}</div>'
-            
-        except ValueError:
-            numbers = (list(map(str, request_body['numbers'][0].split(' '))))
-            response_body += f'<div>{", ".join(list(map(str, numbers)))}</div>'
-            response_body += f'<div>Error: Enter numbers</div>'
-            request_body = parse_qs(request.body.decode())
+        player_nums = []
+        bulls = []
+        cows = []
 
+        for key in numbers_dict:
+            player_nums.append(int(numbers_dict[key]))
+        print("===== SECRET NUMS =====: ", str(secret_nums))
+        print(">>>>> PLAYER NUMS >>>>>: ", str(player_nums))
+
+        if player_nums == secret_nums:
+            return render(request, 'win.html', numbers_dict) 
         
-        return render(request, 'result_history.html', context_calc) 
+
+        for el in player_nums:
+            try:
+                if player_nums.index(el) == secret_nums.index(el):
+                    bulls.append(el)
+                    numbers_dict['bulls'] = len(bulls)
+
+                else:
+                    cows.append(el)
+                    numbers_dict['cows'] = len(cows)
+
+            except ValueError:
+                pass
+
+        return render(request, 'result_history.html', numbers_dict)
